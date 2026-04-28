5ee885ea086505386c902f5344c751da7331d251
nearmint <273466063+nearmint@users.noreply.github.com>
2026-04-28 07:57:25 +0200

    fix(i18n): correct invalid __(%s) call (QC-012)
    - printf(__('%s'), get_comment_author_link()) is meaningless:
      '%s' as a translatable string has no semantic value
    - Replace with direct echo (output already escaped via WP)

--- functions.php
+++ functions.php
@@ -179,7 +179,7 @@ function tape_comment($comment, $args, $depth) {
             if ( $args['avatar_size'] != 0 ) {
                 echo get_avatar( $comment, $args['avatar_size'] );
             }
-            printf( __( '%s' ), get_comment_author_link() ); ?>
+            echo get_comment_author_link(); ?>
         </small></p><?php
         if ( $comment->comment_approved == '0' ) { ?>
             <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em><br/><?php
